f72b09afd036bb6194894b17bc5d9ab00c119939
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-15 22:14:57 +0300

    Prepared for release at: 2020-07-15 22:14:56

--- R/agepyramid.b.R
+++ R/agepyramid.b.R
@@ -17,7 +17,7 @@ agepyramidClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
 
             # Error Message ----
-            if ( (is.null(self$options$age) && is.null(self$options$gender)) )
+            if ( (is.null(self$options$age) || is.null(self$options$gender)) )
                 return()
 
             if (nrow(self$data) == 0) stop("Data contains no (complete) rows")
