5af0fbe05ddfd49a9c51b1edc173a8a2d340fc60
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-17 17:51:08 +0300

    Prepared for release at: 2020-07-17 17:51:08

--- R/alluvial.b.R
+++ R/alluvial.b.R
@@ -135,7 +135,7 @@ alluvialClass <- if (requireNamespace('jmvcore')) R6::R6Class(
 
             if (themex == "jamovi") {
                 plot <- plot + ggtheme
-            } else if (themex == "easyalluvial") {
+            } else if (marg || themex == "easyalluvial") {
                 plot <- plot
             # } else if (themex == "ipsum") {
             #     plot <- plot + hrbrthemes::theme_ipsum()
