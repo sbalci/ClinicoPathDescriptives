0ca87432f1d3d1a151ed7c80a9393f961b638944
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-10 10:24:03 +0300

    WIP at: 2020-08-10 10:24:03

---
+++ R/lollipop.b.R
@@ -0,0 +1,66 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+lollipopClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "lollipopClass",
+    inherit = lollipopBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+
+
+
+
+            # plotData <- self$data
+
+            # image <- self$results$plot
+            # image$setState(plotData)
+
+
+
+
+        }
+
+
+        ,
+        .plot = function(image, ggtheme, theme, ...) {
+
+
+            # Error Message ----
+            if ( (is.null(self$options$age) || is.null(self$options$gender)) )
+                return()
+
+            if (nrow(self$data) == 0) stop("Data contains no (complete) rows")
+
+
+            # read data ----
+
+            # plotData <- image$state
+
+
+            plotData <- self$data
+
+            group <- self$options$group
+            dep <- self$options$dep
+
+            plot <-
+            plotData %>%
+            ggcharts::lollipop_chart(x = group
+                                     , y = dep
+                                     # , threshold = 30
+                                     )
+
+
+            # plot <- plot + ggtheme
+
+            print(plot)
+            TRUE
+
+        }
+
+        )
+)
