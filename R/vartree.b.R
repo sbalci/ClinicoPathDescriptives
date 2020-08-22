4dce5d9671fd93379fdbbda8f1f52b44bf2df8cf
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-22 21:55:07 +0300

    WIP at: 2020-08-22 21:55:07

---
+++ R/vartree.b.R
@@ -0,0 +1,60 @@
+#' @title Variable Tree
+#'
+#' @importFrom R6 R6Class
+#' @import jmvcore
+
+vartreeClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "vartreeClass",
+    inherit = vartreeBase,
+    private = list(
+        .run = function() {
+
+
+            if ( is.null(self$options$vars) ) {
+                # ToDo Message ----
+                todo <- "
+                <br>Welcome to ClinicoPath DEscriptives Module
+                          <br><br>
+                          This tool will help you form a Variable Tree.
+                          "
+                html <- self$results$todo
+                html$setContent(todo)
+
+            } else {
+                todo <- ""
+                html <- self$results$todo
+                html$setContent(todo)
+
+            }
+
+            # Error Message ----
+
+            if (nrow(self$data) == 0) stop("Data contains no (complete) rows")
+
+            # Read Data ----
+
+            mydata <- self$data
+
+            mydata <- jmvcore::naOmit(mydata)
+
+            formula <- jmvcore::constructFormula(terms = self$options$vars)
+
+            myvars <- jmvcore::decomposeFormula(formula = formula)
+
+            myvars <- unlist(myvars)
+
+            mydata <- mydata %>%
+                dplyr::select(myvars)
+
+            myvars <- paste0(myvars, collapse = " ")
+
+            results <- vtree::vtree(mydata, myvars)
+
+            results <- DiagrammeRsvg::export_svg(gv = results)
+
+            self$results$text1$setContent(print(results))
+
+        }
+
+        )
+)
