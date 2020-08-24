ab8fc4545582dbb6a11939d7a78a2d1f9264a8d6
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-24 19:59:27 +0300

    WIP at: 2020-08-24 19:59:27

--- R/vartree.b.R
+++ R/vartree.b.R
@@ -152,8 +152,7 @@ vartreeClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             # Percentage Variable ----
             if ( !is.null(self$options$percvar) ) {
                 percvar <- self$options$percvar
-                xsummary <- paste0(percvar, "=", self$options$percvarLevel ,"\n%pct%")
-                xsummary <- paste(percvar, "\n", xsummary)
+                xsummary <- paste(percvar,"=", self$options$percvarLevel ,"\n%pct%")
             }
 
 
