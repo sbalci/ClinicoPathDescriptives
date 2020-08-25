58ae05832d9d542a07e637a00a8c7fc2a165f051
SERDAR BALCI <drserdarbalci@gmail.com>
2020-08-25 17:30:11 +0300

    WIP at: 2020-08-25 17:30:11

--- R/vartree.b.R
+++ R/vartree.b.R
@@ -281,7 +281,7 @@ vartreeClass <- if (requireNamespace('jmvcore')) R6::R6Class(
                 # showlpct = TRUE,
                 showcount = self$options$showcount,
                 # showlegend = FALSE,
-                # varnamepointsize = 18,
+                varnamepointsize = 10,
                 # HTMLtext = FALSE,
                 # digits = 0,
                 # cdigits = 1,
